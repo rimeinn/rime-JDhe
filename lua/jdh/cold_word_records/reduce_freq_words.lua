af092dcc37cf7e3f7968d6d28f62e1c548d3626c
zcz <11059864+hk21002@user.noreply.gitee.com>
2024-12-29 20:36:19 +0800

    修改自造词
    1 2简改动

--- lua/jdh/cold_word_records/reduce_freq_words.lua
+++ lua/jdh/cold_word_records/reduce_freq_words.lua
@@ -1,4 +1,17 @@
 local reduce_freq_words =
-{ 	["颜射"] = { "yjue", },
+{ 	["下"] = { "x", },
+	["叭"] = { "bao", },
+	["侧"] = { "cn", },
+	["芒"] = { "mhzr", },
+	["寻"] = { "xy", },
+	["摇"] = { "yc", },
+	["傻逼"] = { "sb", },
+	["夜视"] = { "yeui", },
+	["德"] = { "de", },
+	["qBittorrentEnhancedEdition"] = { "qe", },
+	["如何"] = { "rh", },
+	["颜射"] = { "yjue", },
+	["仙术"] = { "xmuu", },
+	["都"] = { "d", },
 }
 return reduce_freq_words
