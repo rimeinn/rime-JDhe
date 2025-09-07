3b86861fdc06a634dd5fa8a62e3352e5f6f00701
GGboxCloud <140216532+GGboxCloud@users.noreply.github.com>
2025-09-07 22:37:53 +0800

    出简让全更新

--- lua/moran_ijrq_filter.lua
+++ lua/moran_ijrq_filter.lua
@@ -2,16 +2,45 @@
 --
 -- Part of Project Moran
 -- License: GPLv3
--- Version: 0.1.0
+-- Version: 0.3.0
+
+-- ChangeLog:
+--
+-- 0.3.0: 增加 enable_word_defer 選項，若首選應下沉，則移動首選到該數
+--        目個候選之後。
+--
+-- 0.2.0: 增加 enable_word_delay 選項。若用戶非常熟悉輔助碼，可能會在
+--        輸入時直接打出輔助碼，這時出簡讓全的效果反而不是用戶期望的。
+--
+-- 0.1.0: 實作
 
 local moran = require("moran")
 local Module = {}
 
 function Module.init(env)
    env.enabled = env.engine.schema.config:get_bool("moran/ijrq/enable_word")
+   env.delay = env.engine.schema.config:get_int("moran/ijrq/enable_word_delay") or 0
+   env.defer = env.engine.schema.config:get_int("moran/ijrq/enable_word_defer") or 1
+
+   if env.enabled and (type(env.defer) ~= "number" or env.defer % 1 ~= 0 or env.defer <= 0) then
+      log.error("moran/enable_word_defer is not an integer >= 1! ijrq_filter is automatically disabled.")
+      env.enabled = false
+   end
+
+   -- Debouncer
+   env.last_timestamp = 0
+   if rime_api.get_time_ms == nil then
+      env.get_time_ms = function()
+         return 0
+      end
+   else
+      env.get_time_ms = rime_api.get_time_ms
+   end
 end
 
 function Module.fini(env)
+   env.last_input = nil
+   env.last_first_cand = nil
 end
 
 function Module.func(t_input, env)
@@ -44,6 +73,7 @@ function Module.func(t_input, env)
       end
       env.last_input = input
       env.last_first_cand = first_cand:get_genuine().text
+      env.last_timestamp = env.get_time_ms()
       yield(first_cand)
       moran.yield_all(iter)
    elseif input_len == 5 or input_len == 7 or input_len == 9 then
@@ -52,41 +82,84 @@ function Module.func(t_input, env)
          return
       end
 
+      -- If the user types auxcode super fast, then we should NOT
+      -- attempt to postpone first cand.
+      if not Module.debounce(env) then
+         moran.yield_all(iter)
+         return
+      end
+
+      -- FIXME: The code is a mess.
+      -- Extend Yielder to support "floating" defer.
       local postpone = false
+      local initset = {}
       local pset = {}
-      local real_first_cand = nil  -- actually, it is only REAL if it is in the same GROUP
+      local defer = env.defer - 1
       for c in iter do
          -- a genuine cand may generate multiple cands
          local g = c:get_genuine()
          if g.text == env.last_first_cand then
             table.insert(pset, c)
-         else
-            real_first_cand = c
+         elseif defer > 0 then
+            table.insert(initset, c)
+            defer = defer - 1
+         elseif defer == 0 then
+            table.insert(initset, c)
             break
          end
       end
 
-      if real_first_cand then
-         if utf8.len(real_first_cand.text) == utf8.len(env.last_first_cand) then
-            -- same group!
-            postpone = true
+      -- yield candidates in the same group as last_first_cand
+      local skipped = 0
+      for i, c in ipairs(initset) do
+         if utf8.len(c.text) == utf8.len(env.last_first_cand) then
+            -- same group, yield first
+            yield(c)
+            skipped = skipped + 1
+            initset[i] = nil
          else
-            postpone = false
+            break
          end
       end
 
-      if postpone then
-         if real_first_cand then yield(real_first_cand) end
-         for _,c in pairs(pset) do yield(c) end
-         for c in iter do yield(c) end
-      else
-         for _,c in pairs(pset) do yield(c) end
-         if real_first_cand then yield(real_first_cand) end
-         for c in iter do yield(c) end
+      -- yield deferred candidates
+      for _,c in pairs(pset) do
+         yield(c)
       end
+
+      -- yield other candidates
+      for i = skipped + 1, #initset do
+         yield(initset[i])
+      end
+      for c in iter do
+         yield(c)
+      end
+      
+
+      -- if postpone then
+      --    if real_first_cand then yield(real_first_cand) end
+      --    for _,c in pairs(pset) do yield(c) end
+      --    for c in iter do yield(c) end
+      -- else
+      --    for _,c in pairs(pset) do yield(c) end
+      --    if real_first_cand then yield(real_first_cand) end
+      --    for c in iter do yield(c) end
+      -- end
    else
       moran.yield_all(iter)
    end
+end
+
+--| Returns true if the current invocation is a new invocation
+-- (i.e. not too close to the last invocation).
+function Module.debounce(env)
+   if env.delay == nil or env.delay < 1 then
+      return true
+   end
+   local cur = env.get_time_ms()
+   local last = env.last_timestamp
+   env.last_timestamp = cur
+   return cur - last >= env.delay
 end
 
 return Module
